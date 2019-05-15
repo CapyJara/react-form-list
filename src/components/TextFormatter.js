5c4334937ce7df5843d9553420890b31e382ebe3
UncleJerry <jared.myhrberg@gmail.com>
2019-05-15 14:22:43 -0700

    text formatter working

---
+++ src/components/TextFormatter.js
@@ -0,0 +1,22 @@
+import React, { PureComponent } from 'react';
+
+export default class TextFormatter extends PureComponent {
+state = {
+  text: '',
+};
+
+handleChange = ({ target }) => {
+  this.setState({ [target.name]: target.value });
+}
+
+render() {
+  const { text } = this.state;
+   
+  return (
+    <>
+      <input type="text" onChange={this.handleChange}></input>
+      <p>{text}</p>
+    </>
+  );
+}
+}
